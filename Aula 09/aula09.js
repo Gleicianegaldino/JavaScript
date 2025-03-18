0c7d390cfae39f34ff4dae8b8aa56dc5ceb71774
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-18 11:11:29 -0300

    Pré e Pós Incremento

---
+++ Aula 09/aula09.js
@@ -0,0 +1,21 @@
+let  n=10
+
+//pós-incremento
+//n++//incrementa + 1  (pega o valor(usa) e depois incrementa)
+//console.log(n++) //não funcionar 
+
+
+//pré-incremento + 1 (incrementa e depois pega o valor(usa))
+//console.log(++n) //funcionar nesse caso
+
+//n++
+//n+=10
+//n=-10 //o sinal - é operador de inversão/negação
+
+//--n
+//console.log(n)
+
+let x=20
+
+console.log(n + x) //soma
+console.log(n + "" + x) //concatena (+ associado a string vira concatenação)
