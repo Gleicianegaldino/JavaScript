9193524f2a609c1c3bbf4f8e56bc2d5f5783f339
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-18 11:12:15 -0300

    Operador ternário

---
+++ Aula 10/aula10.js
@@ -0,0 +1,31 @@
+// let num=11
+
+// let res=num%2
+
+// if(res==0){
+//     console.log("Par")
+// }else{
+//     console.log("Impar")
+// }
+
+
+//? operador ternário -> primeiro valor(verdadeiro) : segundo valor(falso)
+//teste lógico ? se verdadeiro : se falso
+
+// let num=10
+// let res=(!(num%2) ? "Par" : "Impar") 
+
+// console.log(res)
+
+
+// let num1=10,num2=50
+
+// //let res= num1 > num2
+// let res=((num1 > num2) ? "Sim, é maior" : "Não, é menor")
+
+// console.log(res)
+
+let status="B"
+let res=((status == "A") ? "Ativo" : "Inativo" )
+
+console.log(res)
