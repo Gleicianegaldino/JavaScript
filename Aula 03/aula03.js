ab7672adc81209c52801dd890e4e8fb4ad55ee9f
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-18 11:05:36 -0300

    LET, VAR, CONST

---
+++ Aula 03/aula03.js
@@ -0,0 +1,32 @@
+"use strict"
+
+/*if(true){
+    var nome="Marcos"
+}
+
+console.log(nome)*/
+
+function teste(){
+    let nome="João"
+    if(true){
+        //let nome="João"
+        console.log("dentro do if: " + nome)
+    }    
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+console.log("fora de tudo: " + nome)
+
+/*
+let conv="Saori"
+conv=10
+
+console.log(conv)
+*/
+
+
+/*
+const altera="Não altera"
+console.log(altera)
+*/
