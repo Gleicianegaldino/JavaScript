33c98e0fd91a77a5d08614da9818f3672297703c
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-18 11:07:30 -0300

    Operadores Relacionais

---
+++ Aula 05/aula05.js
@@ -0,0 +1,13 @@
+let num1=10,num2=5,num3=10
+
+console.log(num1 > num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+console.log(num1 == num2)
+console.log(num1 == num3)
+console.log(!(num1 == num2))
+console.log(!(num1 != num3))
+
+console.log(!(num1 == num3)) //not
+console.log(num1 != num3)//diferente
+
