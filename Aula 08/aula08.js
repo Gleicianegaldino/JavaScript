df6ce216df83658465595479b676ec2b2f141e60
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-18 11:10:20 -0300

    Operadores Bitwise

---
+++ Aula 08/aula08.js
@@ -0,0 +1,25 @@
+
+// let n1=11,n2=10
+// let res = n1 & n2
+
+// console.log(res)
+
+
+// let n1=10,n2=11
+// let res= n1 | n2 //onde tiver o bit 1 retorna ele
+
+// console.log(res)
+
+// let n1=13, n2=14
+// let res = n1 ^ n2 //onde tiver equivalência 1+1 retorna 0
+
+// console.log(res)
+
+let n1=20
+//let res= n1 << 1 //desloca 1 bit para esqueda -> dobrar valor
+let res= n1 << 2 //desloca 2 bit para esqueda
+
+//let res= n1 >> 1 //desloca 1 bit para direita -> reduz o valor pela metade
+//let res= n1 >> 2 //desloca 2 bit para direita
+
+console.log(res)
