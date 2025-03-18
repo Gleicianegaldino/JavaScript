a345f9ebe101f4cd9c8221b49cfbb1014fcf7e8e
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-18 11:06:40 -0300

    Operadores Matemáticos

---
+++ Aula 04/aula04.js
@@ -0,0 +1,45 @@
+//let num1=10
+//let num2=5
+
+//console.log(num1)
+//console.log(num2)
+
+/*
+let num1=10,num2=5
+
+console.log(num1)
+console.log(num2)
+*/
+
+/*
+let num1,num2,num3
+
+num1=num2=num3=150
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+*/
+
+let num1=0,num2=0,res=0
+
+num1=15
+num2=10
+
+//console.log(num1-num2)
+//res=num1+num2
+//res=(num1+num2)*2
+//res=num1+num2*2
+//res=num1/num2
+//res=num1%num2
+//num1++
+/*
+num1--
+num1--
+num1--
+num1--
+*/
+//num1+=1 //++
+
+num1*=2
+console.log(num1)
