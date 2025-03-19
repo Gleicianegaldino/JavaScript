be579187465fc1fcd0ac01a4d99777bfd056aef3
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-19 19:12:50 -0300

    Condicionais

---
+++ Aula 13/aula13.js
@@ -0,0 +1,35 @@
+let num=8
+
+// if(num > 10){
+//     console.log("Numeral maior que 10")
+//     ///console.log("comando2")
+// }else if(num > 5 ){
+//     console.log("Numeral maior que 6 e 10")
+// }else{
+//     console.log("Numeral menor ou igual a 5")
+// }
+
+
+
+// if(num > 10){
+//     console.log("maior que 10")
+//     if(num > 50) {
+//         console.log(" e numeral maior que 50")
+//     }
+// }else if(num > 5){
+//     console.log("entre 6 e 10")
+// }else {
+//     console.log("igual ou menor que 5")
+// }
+
+
+let energia=100
+//let clima="sol"
+let clima="chovendo"
+
+
+if(energia > 70 && clima == "sol"){
+    console.log("vou a praia")
+}else {
+    console.log("vou ao cinema")
+}
