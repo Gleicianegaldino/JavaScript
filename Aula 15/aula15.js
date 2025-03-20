f5b7865719b00eb32804273d2a3c24fefd810d12
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-20 11:37:30 -0300

    Loop FOR

---
+++ Aula 15/aula15.js
@@ -0,0 +1,11 @@
+// for(let i=0; i < 10; i++){
+//     console.log("valor de i: " +i)
+// }
+
+for(let i=0; i < 30; i++){
+    if(i%2==0){
+        console.log("Valor " + i + " é par")
+    }else {
+        console.log("Valor " + i + " é impar")
+    }
+}
