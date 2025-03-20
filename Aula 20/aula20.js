b9cf4cd96d9871322fe56ca216e7e5e706b29622
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-20 11:41:10 -0300

    Funções p1

---
+++ Aula 20/aula20.js
@@ -0,0 +1,33 @@
+// function nome(){
+//     console.log("saori")
+// }
+// nome()
+
+
+// for(let i=0;i<10;i++){
+//     nome()
+// }
+
+// function soma2_10(){
+//     let n1=2, n2=10, soma=n1+n2
+//     console.log(soma)
+// }
+// soma2_10()
+
+
+// for(let i=0;i<10;i++){
+//     soma2_10()
+// }
+
+
+
+function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="aiaiai"
+    d2.innerHTML="uiuiuiuiui"
+    d3.innerHTML="oioioio"
+}
+
+//mudarTexto()
