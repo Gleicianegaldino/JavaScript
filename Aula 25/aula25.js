e7335a03066cea42b40da21138893404fac687c4
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-21 11:21:03 -0300

    Arrow Function

---
+++ Aula 25/aula25.js
@@ -0,0 +1,16 @@
+// const soma=(v1,v2)=>{
+//     return v1+v2
+// }
+// console.log(soma(10,5))
+
+const soma=(v1,v2)=>{
+    let res=v1+v2
+    return res
+}
+console.log(soma(10,5))
+
+const nome=n=>{return n}
+console.log(nome("nomeee"))
+
+const add=n=>n+10 //quando não usa o return não é obrigado usar as {} - isso somente quando tiver apenas 1 tinha dentro da arrow function
+console.log(add(15))
