bb0ba18bc12dbc7c2fab50d3d2fa8e210586884f
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2025-03-21 09:16:20 -0300

    Funções p1

--- Aula 20/aula20.js
+++ Aula 20/aula20.js
@@ -1,18 +1,33 @@
-function canal(){
-    let n1=10, n2=2, res=n1*n2
-    //console.log("cursos")
-    //return "retornando valores para quem chamou"
-
-    if(res%2==0){
-        return "Par"
-    }else{
-        return "Impar"
-    }
-    //return res
+// function nome(){
+ //     console.log("saori")
+ // }
+ // nome()
+ 
+ 
+ // for(let i=0;i<10;i++){
+ //     nome()
+ // }
+ 
+ // function soma2_10(){
+ //     let n1=2, n2=10, soma=n1+n2
+ //     console.log(soma)
+ // }
+ // soma2_10()
+ 
+ 
+ // for(let i=0;i<10;i++){
+ //     soma2_10()
+ // }
+ 
+ 
+ 
+ function mudarTexto(){
+    let d1=document.getElementById("d1")
+    let d2=document.getElementById("d2")
+    let d3=document.getElementById("d3")
+    d1.innerHTML="aiaiai"
+    d2.innerHTML="uiuiuiuiui"
+    d3.innerHTML="oioioio"
 }
 
-let num=canal()
-
-//canal()
-//console.log(canal())
-console.log("retorno do let num: " + num)
+//mudarTexto()
